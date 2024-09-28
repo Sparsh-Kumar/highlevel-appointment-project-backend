d618a4527ae61784a7992218b306413e13465c02
Sparsh-Kumar <sparshkumar14998@gmail.com>
2024-09-28 14:04:22 +0530

    linting issue resolved.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,5 @@
-const gulp = require("gulp");
-const ts = require("gulp-typescript");
-const tsProject = ts.createProject("tsconfig.json");
-gulp.task("default", function () {
-  return tsProject.src().pipe(tsProject()).js.pipe(gulp.dest("dist"));
-});
+const gulp = require('gulp');
+const ts = require('gulp-typescript');
 
+const tsProject = ts.createProject('tsconfig.json');
+gulp.task('default', () => tsProject.src().pipe(tsProject()).js.pipe(gulp.dest('dist')));
